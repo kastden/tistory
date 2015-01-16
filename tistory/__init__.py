cadd4aa2bed67cad605937788d58e598ab1cdfc8
K. Ast Den <post@kastden.org>
2015-01-16 04:47:39 +0100

    Remove load_config() from the tistory module

--- tistory/__init__.py
+++ tistory/__init__.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
 
-import json
-import os
-from shlex import quote as shlex_quote
-
 from tistory.api import Tistory, TistoryError
 from tistory.auth import TistoryOAuth2
-
-
-def load_config(fname):
-    fname = shlex_quote(fname)
-    cf_path = os.path.dirname(os.path.realpath(__file__))
-    abspath = os.path.abspath(os.path.join(cf_path, '../config/', fname))
-
-    with open(abspath) as data:
-        config = json.loads(data.read())
-
-    return config
 
 if __name__ == "__main__":
     pass
